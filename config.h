703068222121be2d905675e4e21a9fd6383606e7
bash-win <ash.b.dev@gmail.com>
2026-02-16 12:42:51 -0500

    Add alpha to selection

--- config.h
+++ config.h
@@ -146,6 +146,12 @@ unsigned int defaultbg = 0;
 unsigned int defaultcs = 7;
 static unsigned int defaultrcs = 7;
 
+unsigned int selectionbg = 8;
+unsigned int selectionfg = 7;
+/* If 0 use selectionfg as foreground in order to have a uniform foreground-color */
+/* Else if 1 keep original foreground-color of each cell => more colors :) */
+static int ignoreselfg = 1;
+
 /*
  * Default shape of cursor
  * 2: Block ("█")
